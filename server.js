f51a0653ce1d0f3757ff16eda8da8509a864f61f
Victor <viajedeestudiosvictor@outlook.com>
2024-10-21 23:11:01 +0200

    Configuración de servidor y estructura de archivos

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
+
 const path = require('path');
-const nodemailer = require('nodemailer');
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join('webviaje', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/index', (req, res) => {
-    res.sendFile(path.join('webviaje', 'public', 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'productos.html'));
 });
 
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join('webviaje', 'public', 'carrito.html'));
-});
-app.get('/productos', (req, res) => {
-    res.sendFile(path.join('webviaje', 'public', 'productos.html'));
+    res.sendFile(path.join(__dirname, 'public', 'carrito.html'));
 });
 
-app.listen(PORT, (3000) => {
+app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
+
