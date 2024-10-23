6e6f0e23a868d39cd537a150e0d715208e39e6e8
Victor <viajedeestudiosvictor@outlook.com>
2024-10-23 20:28:50 +0200

    Actualizar server.js con la funcionalidad de email

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -18,36 +19,29 @@ app.get('/carrito', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'carrito.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
-});
-
 app.post('/send-email', (req, res) => {
     const { email, name, total, purchaseCode, delivery, observations, cartItems } = req.body;
-
     const transporter = nodemailer.createTransport({
         service: 'smtp.outlook.live.com',
         auth: {
-            user: 'viajedeestudiosvictor@outlook.com', 
-            pass: 'AndresZapataRamirez1230' 
+            user: 'viajedeestudiosvictor@outlook.com',
+            pass: 'AndresZapataRamirez1230'
         },
     });
 
     const emailBody = `
         ¡Gracias por tu colaboración, ${name}!
-
         Monto total: ${total}€
         Código de compra: **${purchaseCode}**
-        
+
         Instrucciones para pago por Bizum: 632051554
         Por favor, realiza un Bizum a este número indicando en el motivo de pago tu nombre y apellidos junto con el código de compra.
 
         Lugar de entrega: ${delivery}
         Observaciones: ${observations}
-
         Detalle de productos:
         ${cartItems.map(item => `- ${item.name}: ${item.price}€`).join('\n')}
-
+        
         **Nos pondremos en contacto contigo para ultimar la entrega.**
         Y de nuevo, **¡Gracias por no dejarme tirado en la carretera!**
     `;
@@ -55,7 +49,7 @@ app.post('/send-email', (req, res) => {
     const emailOptions = {
         from: 'viajedeestudiosvictor@outlook.com',
         to: email,
-        cc: 'viajedeestudiosvictor@outlook.com', 
+        cc: 'viajedeestudiosvictor@outlook.com',
         subject: 'Instrucciones para pagar tu compra',
         text: emailBody,
     };
@@ -63,16 +57,16 @@ app.post('/send-email', (req, res) => {
     transporter.sendMail(emailOptions, (error, info) => {
         if (error) {
             console.error('Error al enviar el email:', error);
-            if (!res.headersSent) {
-                return res.status(500).send('Error al enviar el email.');
-            }
+            return res.status(500).send('Error al enviar el email.');
         } else {
             console.log('Email enviado: ' + info.response);
-            if (!res.headersSent) {
-                return res.status(200).send('Email enviado con éxito.');
-            }
+            return res.status(200).send('Email enviado con éxito.');
         }
     });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
 
 
