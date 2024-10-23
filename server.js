23a8c6ad60b5c1ef1f8cfbce23373d6a363eccc4
Victor <viajedeestudiosvictor@outlook.com>
2024-10-23 18:17:22 +0200

    Actualizar server.js con la funcionalidad de email

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/send-email', (req, res) => {
     const { email, name, total, purchaseCode, delivery, observations, cartItems } = req.body;
 
     const transporter = nodemailer.createTransport({
-        service: 'Outlook365',
+        service: 'Outlook',
         auth: {
             user: viajedeestudiosvictor@outlook.com, 
             pass: AndresZapataRamirez1230, 
